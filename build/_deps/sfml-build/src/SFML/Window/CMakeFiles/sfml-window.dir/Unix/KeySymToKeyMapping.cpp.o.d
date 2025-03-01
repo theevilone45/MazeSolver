846a659d9c4bd74b65ff5c856dc5a9dff64684d2
Marcin Saffenreuther <theevilone45@gmail.com>
2025-03-01 01:24:25 +0100

    [clean-up] remove compile_commands.json, .cache, build directories

--- build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o: \
- /home/marcins/FunctionalMaze/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.cpp \
- /usr/include/stdc-predef.h \
- /home/marcins/FunctionalMaze/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.hpp \
- /home/marcins/FunctionalMaze/build/_deps/sfml-src/include/SFML/Window/Keyboard.hpp \
- /home/marcins/FunctionalMaze/build/_deps/sfml-src/include/SFML/Window/Export.hpp \
- /home/marcins/FunctionalMaze/build/_deps/sfml-src/include/SFML/Config.hpp \
- /usr/include/X11/X.h /usr/include/X11/keysym.h \
- /usr/include/X11/keysymdef.h
